11c16d25bcf97839b08255fed9c2c9d3a0f9e19d
Lukas H <lukas@ideallygrey.uk>
2023-05-20 19:57:14 -0400

    Moved interpret to function, fixed issue

--- macbeth-game/scenes/main.gd
+++ macbeth-game/scenes/main.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
+func interpret_current_line():
+		# If next line begins with "!", move to that line, set image to int. following the "!"
+		if GV.game_script[GV.current_line + 1].begins_with("!"):
+			GV.current_line += 1
+			GV.current_image = GV.game_script[GV.current_line].to_int()
+		# Move to next line, set on-screen text to the line
+		GV.current_line += 1
+		GV.on_screen_text = GV.game_script[GV.current_line]
+
+# Runs once when first loaded, so that game doesn't open to blank screen/text
 func _ready():
-	pass
+	interpret_current_line()
 
 # On every frame
 func _process(_delta):
 	# If space/left mouse is pressed, and next line is valid
 	if Input.is_action_just_pressed("ui_continue") and GV.current_line + 1 < GV.game_script.size():
-		# If next line begins with "!", move to that line, set image to int following the "!"
-		if GV.game_script[GV.current_line + 1].begins_with("!"):
-			GV.current_line += 1
-			GV.current_image = GV.game_script[GV.current_line].to_int()
-		# Move to next line, set on screen text to the line
-		GV.current_line += 1
-		GV.on_screen_text = GV.game_script[GV.current_line]
+		interpret_current_line()
