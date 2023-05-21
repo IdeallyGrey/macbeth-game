7be831228d4995557b1e5962384bd85bff72e616
Lukas H <lukas@ideallygrey.uk>
2023-05-20 22:27:09 -0400

    Can't skip text, setup empty game_over func

--- macbeth-game/gdscript/main.gd
+++ macbeth-game/gdscript/main.gd
@@ -12,12 +12,23 @@ func interpret_current_line():
 		GV.current_line += 1
 		GV.on_screen_text = GV.game_script[GV.current_line]
 
+func game_over():
+	pass
+
 # Runs once when first loaded, so that game doesn't open to blank screen/text
 func _ready():
 	interpret_current_line()
 
 # On every frame
 func _process(_delta):
-	# If space/left mouse is pressed, next line is valid, and text is not currently being loaded
-	if Input.is_action_just_pressed("ui_continue") and GV.current_line + 1 < GV.game_script.size() and not GV.currently_typing:
-		interpret_current_line()
+	# If space/left mouse is pressed and text is not currently being loaded
+	if Input.is_action_just_pressed("ui_continue") and not GV.currently_typing:
+		# Check if there are more lines
+		if GV.current_line + 1 < GV.game_script.size():
+			interpret_current_line()
+		else:
+			game_over()
+
+
+
+
